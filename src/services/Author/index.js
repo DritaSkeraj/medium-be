e8cc820efc30a1091a14be264bb1608595c229ba
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-18 01:47:27 +0100

    crud for authors

--- src/services/Author/index.js
+++ src/services/Author/index.js
@@ -13,4 +13,59 @@ router.post('/', async (req, res, next) => {
     }
 })
 
+router.get("/", async(req, res, next) => {
+    try{
+        const authors = await AuthorsModel.find(req.query)
+        res.send(authors)
+    } catch(error){
+        console.log(error)
+        next(error)
+    }
+})
+
+router.get("/:id", async(req, res, next) => {
+    try{
+        const id = req.params.id;
+        const author = await AuthorsModel.findById(id)
+        if(author){
+            res.send(author)
+        } else {
+            next("i made a BooBoo")
+        }
+    } catch (error){
+        console.log(error)
+        next(error)
+    }
+})
+
+router.put("/:id", async (req, res, next) => {
+    try {
+      const author = await AuthorsModel.findByIdAndUpdate(req.params.id, req.body)
+      if (author) {
+        res.send("Ok")
+      } else {
+        const error = new Error(`author with id ${req.params.id} not found`)
+        error.httpStatusCode = 404
+        next(error)
+      }
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  router.delete("/:id", async (req, res, next) => {
+    try {
+      const author = await AuthorsModel.findByIdAndDelete(req.params.id)
+      if (author) {
+        res.send("Deleted")
+      } else {
+        const error = new Error(`author with id ${req.params.id} not found`)
+        error.httpStatusCode = 404
+        next(error)
+      }
+    } catch (error) {
+      next(error)
+    }
+  })
+
 module.exports = router
